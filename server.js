cfc7965385f275a2aef9f2eb26da3cfeb261c628
ioanniswd <ioanniswd@gmail.com>
2016-12-18 18:49:07 +0200

    Second commit

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 var express = require("express");
+var path = require('path');
 var app = express();
 
 console.log('hi');
 
+app.use(express.static(path.join(__dirname, '/css')));
+
+app.get('/', function(req,res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
 app.get('/:time', function(req, res) {
-   res.send('Hi there' + req.params.time); 
+
+	var time = req.params.time;
+	var resDate;
+
+	if(Number(time)) {
+
+
+		resDate = {'unix': time, "natural": new Date(time*1000).toUTCString()};
+	} else {
+		if(Date.parse(time)) {
+			var tempDate = new Date(time);
+			resDate = {'unix': tempDate.getTime()/1000, "natural": tempDate.toUTCString()};
+
+		} else {
+			resDate = {"unix": null, "natural": null};
+		}
+	}
+
+	res.send(resDate);
+
+
+
 });
 
 app.listen(8080, function() {
